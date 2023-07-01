45d9e44f94cd2e2d2c884ff4b5d4c8e8a1e46bf8
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-06-30 20:46:11 -0700

    speed things up with the power of multithreading

--- main.c
+++ main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+
+// Make sure that your compiler supports C11 threads
+#if __STDC_NO_THREADS__ == 0
+#include <threads.h>
+#else
+#error "Threads are not supported for this compiler"
+#endif
+
 #include <raylib.h>
+
 
 #define MAX_ITERATIONS 200
 #define WIDTH 800
 #define HEIGHT 600
+#define NUM_Threads 4
 
 float zoom = 1.0f;
 Vector2 center = {-0.716f, 0.3f};
+
+typedef struct {
+    Image* image;
+    int start;
+    int end;
+} ThreadData;
 
 int mandelbrot(float x, float y) {
     float xx = 0.0f, yy= 0.0f;
@@ -22,6 +38,27 @@ int mandelbrot(float x, float y) {
     return iter;
 }
 
+int drawMandelbrot(void* arg) {
+    ThreadData* data = (ThreadData*) arg;
+
+    for (int x = data->start; x < data->end; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            float cx = ((float)x / WIDTH * 3.5f - 2.5f) * zoom + center.x;
+            float cy = ((float)y / HEIGHT * 2.0f - 1.0f) * zoom + center.y;
+
+            int iterations = mandelbrot(cx, cy);
+
+            Color col = (Color){ iterations % 8 * 32,
+                                 iterations % 16 * 16,
+                                 iterations % 32 * 8, 255};
+
+            ImageDrawPixel(data->image,  x, y, col);
+        }
+    }
+
+    return 0;
+}
+
 int main() {
     printf("Hello, World!\n");
 
@@ -34,24 +71,28 @@ int main() {
     while (!WindowShouldClose()){
         zoom *= 0.895f; // zoom factor
 
-        for (int x = 0; x < WIDTH; ++x){
-            for (int y = 0; y < HEIGHT; ++y){
-                float cx = ((float)x / WIDTH * 3.5f - 2.5f) * zoom + center.x;
-                float cy = ((float)y / HEIGHT * 2.0f - 1.0f) * zoom + center.y;
+        int dx = WIDTH / NUM_Threads;
+        thrd_t thr[NUM_Threads];
+        ThreadData data[NUM_Threads];
 
-                int iterations = mandelbrot(cx, cy);
-                Color col = (Color){iterations % 8 * 32,
-                                    iterations % 16 * 16,
-                                    iterations % 32 * 8, 255};
-
-                ImageDrawPixel(&im, x, y, col);
-            }
+        for (int i = 0; i < NUM_Threads; i++) {
+            data[i].start = 1 * dx;
+            data[i].end = (i == NUM_Threads - 1) ? WIDTH : (i + 1) * dx;
+            data[i].image = &im;
+            thrd_create(&thr[i], drawMandelbrot, &data[i]);
         }
+
+        for (int i = 0; i < NUM_Threads; i ++)
+            thrd_join(thr[i], NULL);
+
         UnloadTexture(tex);
         tex = LoadTextureFromImage(im);
 
         BeginDrawing();
+
+        ClearBackground(BLACK);
         DrawTexture(tex, 0, 0, WHITE);
+
         EndDrawing();
     }
 
